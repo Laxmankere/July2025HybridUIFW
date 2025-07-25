ff881b6c315b67199ff9f0394f7bf03b8c7bae09
Lakshaman Kere <111192180+Laxmankere@users.noreply.github.com>
2025-07-25 19:55:40 +0530

    wait time increased

--- src/test/java/com/qa/opencart/tests/LoginPageTest.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -27,7 +27,7 @@ public class LoginPageTest extends BaseTest {
 	public void loginTest() throws InterruptedException {
 		loginPage.doLogin(prop.getProperty("username"), prop.getProperty("password"));
 		String accountPageTitle = loginPage.getAccountPageTitle();
-		Thread.sleep(4000);
+		Thread.sleep(5000);
 		Assert.assertEquals(accountPageTitle,AppConstants.ACCOUNTS_PAGE_TITLE);
 		
 	}
